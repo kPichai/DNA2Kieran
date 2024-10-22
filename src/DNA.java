e61ba0983564674f4f25ea0d2487aa487bd7a125
kpi <kieran.pichai@menloschool.org>
2024-10-22 16:48:26 -0700

    oasjdaosdjfeo

--- src/DNA.java
+++ src/DNA.java
@@ -19,68 +19,72 @@ public class DNA {
      * TODO: Complete this function, STRCount(), to return longest consecutive run of STR in sequence.
      */
     public static int createBinarySTR(String STR) {
-        int binarySTR = 0b0;
+        int binarySTR = 0;
         for (int i = 0; i < STR.length(); i++) {
-            if (STR.charAt(i) == 'A' || STR.charAt(i) == 'a') {
-                binarySTR = binarySTR << 2;
-                binarySTR += 0b00;
-            } else if (STR.charAt(i) == 'C' || STR.charAt(i) == 'c') {
-                binarySTR = binarySTR << 2;
-                binarySTR += 0b01;
-            } else if (STR.charAt(i) == 'T' || STR.charAt(i) == 't') {
-                binarySTR = binarySTR << 2;
-                binarySTR += 0b10;
-            } else if (STR.charAt(i) == 'G' || STR.charAt(i) == 'g') {
-                binarySTR = binarySTR << 2;
-                binarySTR += 0b11;
+            binarySTR = binarySTR << 2;
+            switch(STR.charAt(i)) {
+                case 'C', 'c' -> binarySTR += 0b01;
+                case 'T', 't' -> binarySTR += 0b10;
+                case 'G', 'g' -> binarySTR += 0b11;
             }
         }
         return binarySTR;
     }
 
-    public static int incrementBinary(int binary, char toAdd) {
-        binary = binary << 2;
-        if (toAdd == 'A' || toAdd == 'a') {
-            binary += 0b00;
-        } else if (toAdd == 'C' || toAdd == 'c') {
-            binary += 0b01;
-        } else if (toAdd == 'T' || toAdd == 't') {
-            binary += 0b10;
-        } else if (toAdd == 'G' || toAdd == 'g') {
-            binary += 0b11;
+    public static int generateMask(int lenOfSTR) {
+        return (1 << (2*lenOfSTR)) - 1;
+    }
+
+    public static int incrementBinary(int binary, char toAdd, int mask) {
+        binary = (binary << 2) & mask;
+
+        switch(toAdd) {
+            case 'C', 'c' -> binary += 0b01;
+            case 'T', 't' -> binary += 0b10;
+            case 'G', 'g' -> binary += 0b11;
         }
+
         return binary;
     }
 
     public static int countMax(String sequence, String STR) {
         int max = 0;
-        int temp = 0;
         int STRBinary = createBinarySTR(STR);
+        int[] repetitions = new int[STR.length()];
         int curSequence = createBinarySTR(sequence.substring(0, STR.length()));
-        Queue<Integer> possibleBeginningSTR = new LinkedList<Integer>();
+        int mask = generateMask(STR.length());
+        int len = sequence.length();
+        int lenSTR = STR.length();
 
-        for (int i = STR.length(); i < sequence.length() - STR.length(); i++) {
-            curSequence = incrementBinary(curSequence, sequence.charAt(i));
+        for (int i = STR.length(); i < len; i++) {
             if (curSequence == STRBinary) {
-                temp += 1;
-                for (int j = 0; j < STR.length(); j++) {
-                    i++;
-                    curSequence = incrementBinary(curSequence, sequence.charAt(i));
-                    if (curSequence == STRBinary) {
-                        if (possibleBeginningSTR.isEmpty() || possibleBeginningSTR.peek() + STR.length() != i) {
-                            possibleBeginningSTR.add(i);
+                repetitions[i % lenSTR] += 1;
+            } else {
+                if (repetitions[i % lenSTR] > max) {
+                    max = repetitions[i % lenSTR];
+                }
+                repetitions[i % lenSTR] = 0;
+
+                if (max * lenSTR > len - i) {
+                    for (int repetitionCount : repetitions) {
+                        if (repetitionCount > 0) {
+                            break;
                         }
                     }
+                    return max;
                 }
-            } else {
-                if (temp > 0 && temp > max) {
-                    max = temp;
-                    temp = 0;
-                }
-                if (!possibleBeginningSTR.isEmpty()) {
-                    i = possibleBeginningSTR.remove();
-                    curSequence = createBinarySTR(sequence.substring(i, i + STR.length()));
-                }
+
+            }
+            curSequence = incrementBinary(curSequence, sequence.charAt(i), mask);
+        }
+
+        if (curSequence == STRBinary) {
+            repetitions[sequence.length() % STR.length()] += 1;
+        }
+
+        for (int repetitionCount : repetitions) {
+            if (repetitionCount > max) {
+                max = repetitionCount;
             }
         }
 
